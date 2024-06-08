80406d0ce61bdbefafdb8f6adf92fd2fcc46e58c
code-raushan <code-raushan@users.noreply.github.com>
2024-06-08 20:43:37 +0530

    feat: fiber server init

--- upload-service/main.go
+++ upload-service/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/code-raushan/video-transcoding-service/upload-service/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main(){
+	app := fiber.New()
+
+	app.Use(logger.New())
+
 
 	fmt.Println("connecting to database")
 	database.ConnectDB()
 	fmt.Println("connected to database")
 
-
-	fmt.Println("upload service implementation with user management")
+	if err := app.Listen(":8500"); err != nil {
+		log.Fatalf("Failed to start HTTP server at port 8500")
+	}
 }
