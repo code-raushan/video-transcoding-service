a9f7b12c5a5a6f638a6cc629995b0794c3682a44
code-raushan <code-raushan@users.noreply.github.com>
2024-06-08 19:29:56 +0530

    add: user model

---
+++ upload-service/models/user.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Id       string    `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
+	FullName string    `gorm:"size:255;not null" json:"full_name"`
+	Email    string    `gorm:"size:255;not null;unique" json:"email"`
+	Password string    `gorm:"size:255; not null" json:"-"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	user.Id = uuid.New().String()
+	return
+}
